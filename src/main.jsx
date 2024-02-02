4da6ce40487e4267c6cc8649dabd5277c2ad154d
Abhishek gupta <07abhishekgupta07@gmail.com>
2024-02-02 19:04:21 +0530

    setup: inital setup files added

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import  ReactDOM  from "react-dom/client";
+import App from "./App";
+import './index.css';
+
+
+let elem= document.getElementById('root');
+ReactDOM.createRoot(elem).render(<App/>);
